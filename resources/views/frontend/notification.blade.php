1d1d69131beb9ed5029469b7546fb072fdecab5f
Thinlley1996 <thinley1996.bhutan@gmail.com>
2021-10-07 23:49:46 +0600

    drc v1 first commit 2

--- resources/views/frontend/notification.blade.php
+++ resources/views/frontend/notification.blade.php
@@ -5,6 +5,14 @@
     <div class="main-home-about">
       <div class="container" class="text-center">
         <h1 class="main-home-he">Notification</h1>
+        <form action="{{ url('/notification') }}" method="get"class="d-flex flex-wrap align-items-center justify-content-end" >
+          <div class="search2">
+          <input type="text" class="searchTerm2" name="notification-search" placeholder="Search DRC's Website">
+            <button type="submit" class="searchButton2">
+              <object data="img/icons/magnifying-glass.svg"class="object-search mt-1"> </object>
+          </button>
+                  </div>
+          </form>
       </div>
     </div>
    </div>
